c58b8b6c0bd7999859e7c1dd87d005e000a7dc10
tarunasthana21 <asthana.tarun1@gmail.com>
2021-10-31 21:31:00 +0530

    added new func sum

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,12 @@
 print("adding all the data processing code related to Demogit")
+
 a=5
 b=6
 def sum(a,b):
   c=a+b
-  return a
-  print("added a new function")
+  return c
+  print(" added new function sum")
+
+
   
 
